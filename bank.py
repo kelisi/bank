95bd7c310983a28b36e82a506bad01fa9b8a69a6
mss <mss90@163.com>
2020-07-14 23:23:21 +0800

    add python format module- autopep8

--- bank.py
+++ bank.py
@@ -1,5 +1,19 @@
+import datetime
 from openpyxl import Workbook
 
 wb = Workbook()
 
-print("HELLO WORLD")
+# grab the active worksheet
+ws = wb.active
+
+# Data can be assigned directly to cells
+ws['A1'] = 42
+
+# Rows can also be appended
+ws.append([1, 2, 3])
+
+# Python types will automatically be converted
+ws['A2'] = datetime.datetime.now()
+
+# Save the file
+wb.save("sample.xlsx")
